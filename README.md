f566c85357bcee471ba713909dfaecc5e9694b54
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2024-03-22 23:41:56 +0300

    #Rekürsif ve Extension Metotlar güzel çalışma idi

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # c_Shap101
+
+***Rekürsif ve Extension Metotlar*** 
+
+Extension ve Rekürsif Metotlar
+Extension(Geniþletilmiþ) Metot Kullanýmý
+Extension Metotlarýn kelime anlamý geniþletilebilir metotlardýr. C# 3.0 ile hayatýmýza giren, ayný kodu defalarca yazmak yerine, bir kere yazýp her yerden eriþebileceðimiz kolay kullanýmý olan metotlardýr.
+
+Extension metot yazarken dikkat etmemiz gereken bir kaç kural vardýr. Þu þekilde :
+
+Static bir class içerisinde static bir metot olarak yazýlmalýlar.
+Extension metod da tanýmlý parametrelerden sadece 1 tanesi this ile tanýmlanabilir.
+Küçük bir örnek ile kullanýmýný görelim.
+
+public static class MyExtensionClass {
+    public static bool IsEventNumber(this int value) {
+        if(value%2==0)
+            return true;
+        else
+            return false;
+    }
+}
+Yukarýdaki örnekte static class içerisinde IsEventNumber adýnda bir extension metot görüyoruz. Metodun kendisi de static keywordü ile tanýmlandý. this keywordünün uygulandýðý parametreye bakarsak, integer ver tipindeki deðiþkenlere uygulanabilir is extension metot olduðunu görüyoruz. Sayýsýnýn çift olup olmamasý durumuna göre de geriye bool bir deðer dönüyor.
+
+Extension metotlarýn kullanýmýný oturtmanýn en kolay yolu öncelikle iþi yapacak olan metodu istenileni karþýlayacak þekilde yazmaktýr. Daha sonradan class ve metodu static yaparak ve geniþletilecek veri tipine baðlý parametrenin baþýna this anahtar kelimesi ekleyerek kullanabilirsiniz.
+
+Yukarýdaki extension metodun örnek kullanýmý ise þu þekildedir.
+
+int deger = 123; bool isEven = deger.IsEvenNumber();
+
+Rekürsif(Yinelemeli) Metot Kullanýmý
+Rekürsif metotlar ilk öðrenildiðinde içselleþtirilmesi ve kullanýlmasý zor metotlardýr. Ama yazýlýmýn ilerleyen yýllarýnda kullanýmý oturur ve bir çok soruna rekürsif metotlarý kullanarak çözüm getirebilirsiniz. O nedenle acele etmemek gerek :)
+
+Çok sýk olmasa da bazen fonksiyonun kendi kendini çaðýrmasýna ihtiyaç duyarýz. For döngüsü karmaþýk bir kod yazarak da bu ihtiyacý karþýlamak mümkün. Rekürsif fonksiyonlar ise tam da bu ihtiyaç için ortaya koyulmuþtur.
+
+Rekürsif fonksiyon kendi kendini çaðýrma yeteneðine sahip olduðu için kullanýrken çok dikkatli olmak gerek. En büyük sorunlardan biri sonsuz döngüye neden olmalý. Bu da System.StackOverflowException olarak bilinen hataya neden olur. Yani belleðin stack bölgesinde bu fonksiyon çaðrýmý için ayrýlan alan tükenmiþtir. Yinelemeli olarak yaptýðýnýz iþe baðlý olarak risk de artar.
+
+Aþaðýdaki örneði inceleyerek ve hatta kaðýt üzerinde yazarak yinelemeli fonksiyonlarýn kullanýmýný daha iyi oturtabilirsiniz.
+
+static int FibonacciSerisi(int sayi)
+{  
+    if (sayi==0)
+        return 0;
+    elseif (sayi==1)
+        return 1;
+    else
+        return FibonacciSerisi(sayi-1)+FibonacciSerisi(sayi-2);
+}
--- c_Shap101/Program.cs
+++ c_Shap101/Program.cs
@@ -4,19 +4,44 @@
     {
         //Test();
 
-        Console.Write("Sayı giriniz");
-        int sayi = Convert.ToInt32(Console.ReadLine());
+        //rekürsif öz yenilemeli
+        //Console.Write("Sayı giriniz");
+        //int sayi = Convert.ToInt32(Console.ReadLine());
 
 
-        Console.Write("Üs giriniz");
-        int us = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Üs giriniz");
+        //int us = Convert.ToInt32(Console.ReadLine());
 
-        Islemler ıslemler = new ();
+        //Islemler ıslemler = new();
 
-        Console.WriteLine(ıslemler.Expo(sayi, us));
+        //Console.WriteLine(ıslemler.Expo(sayi, us));
+        //Console.ReadKey();
+
+        //extension  metotlar
+
+        string ifade = "Musab B. Akan";
+
+      ifade.CheckSpaces()
+            .PrintToScreen();
+
+      ifade.RemoveWithSpaces()
+            .PrintToScreen();
+
+
+        int[] dizi = { 1, 7, 5, 9, 11, 2185, -1 };
+
+        dizi.SortArray();
+        dizi.PrintToScreenNumberListArray();
+
+        ifade.GetFirstCharacter()
+            .PrintToScreen();
+
+
         Console.ReadKey();
 
+
     }
+
     public class Islemler
     {
         public int Expo(int sayi, int us)
@@ -64,5 +89,79 @@
             toplam += dizi;
 
         Console.WriteLine("Ortalama : " + toplam / elemanSayi);
+    }
+}
+public static class Extension
+{
+    private const char Separator = ' ';
+    /// <summary>
+    /// Space is check ?
+    /// </summary>
+    /// <param name="param"></param>
+    /// <returns></returns>
+    public static bool CheckSpaces(this string param)
+    {
+        return param.Contains(Separator);
+    }
+    /// <summary>
+    /// Remove space
+    /// </summary>
+    /// <param name="param"></param>
+    /// <returns></returns>
+    public static string RemoveWithSpaces(this string param)
+    {
+        string[] split = param.Split(Separator);
+        return string.Join(string.Empty, split);
+    }
+
+    public static string MakeUpperCase(this string param)
+    {
+        return param.ToUpper();
+    }
+    public static string MakeLowerCase(this string param)
+    {
+        return param.ToLower();
+    }
+    /// <summary>
+    /// Sort array list.
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static int[] SortArray(this int[] arrList)
+    {
+        Array.Sort(arrList);
+        return arrList;
+    }
+    /// <summary>
+    /// String Print to screen
+    /// </summary>
+    /// <param name="param"></param>
+    public static void PrintToScreen(this string param)
+    {
+        Console.WriteLine(param);
+    }
+    /// <summary>
+    /// Bool Print To Screen
+    /// </summary>
+    /// <param name="param"></param>
+    public static void PrintToScreen(this bool param)
+    {
+        Console.WriteLine(param);
+    }
+
+    public static void PrintToScreenNumberListArray(this int[] arrList)
+    {
+        foreach (int arr in arrList)
+            Console.WriteLine(arr);
+    }
+
+    public static bool IsEvenNumber(this int num)
+    {
+        return num % 2 == 0;
+    }
+
+    public static string GetFirstCharacter(this string str)
+    {    
+        return str.Substring(0, 1);
     }
 }
