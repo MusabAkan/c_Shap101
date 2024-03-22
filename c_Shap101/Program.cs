e8e006ab99504253f1853c511c653c0a409f1712
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2024-03-22 22:43:57 +0300

    Add project files.

---
+++ c_Shap101/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Test();
+
+        Console.Write("Sayı giriniz");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("Üs giriniz");
+        int us = Convert.ToInt32(Console.ReadLine());
+
+        Islemler ıslemler = new ();
+
+        Console.WriteLine(ıslemler.Expo(sayi, us));
+        Console.ReadKey();
+
+    }
+    public class Islemler
+    {
+        public int Expo(int sayi, int us)
+        {
+            if (us < 2)
+                return sayi;
+            return Expo(sayi, --us) * sayi;
+
+        }
+    }
+
+    private static void Test()
+    {
+        //try
+        //{
+        //    Console.WriteLine("Bir sayı giriniz");
+        //    int sayi = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Girmiş olduğunuz sayi " + sayi);
+
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine("Hata :" + ex.Message.ToString());    
+
+        //}
+        //finally
+        //{
+        //    Console.WriteLine("İşlem tamamlandı");
+        //}
+        Console.Write("Dizi eleman sayısı  giriniz: ");
+        int elemanSayi = int.Parse(Console.ReadLine());
+        int[] diziler = new int[elemanSayi];
+
+        for (int i = 0; i < elemanSayi; i++)
+        {
+            Console.Write($"Lütfen {i + 1}. sayısı giriniz : ");
+            diziler[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        Array.Sort(diziler);
+        int toplam = 0;
+
+        foreach (int dizi in diziler)
+            toplam += dizi;
+
+        Console.WriteLine("Ortalama : " + toplam / elemanSayi);
+    }
+}
