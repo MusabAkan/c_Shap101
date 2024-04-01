b275584711cc7b3d20946bfd0e2a9de7aebc68e9
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2024-04-01 22:54:35 +0300

    #üçgen çizme

---
+++ DrawingATriangle/Program.cs
@@ -0,0 +1,8 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int size = UserInput.GetSize();
+        TriangleDrawer.DrawTriangle(size);
+    }
+}
---
+++ DrawingATriangle/UserInput.cs
@@ -0,0 +1,13 @@
+﻿public class UserInput
+{
+    public static int GetSize()
+    {
+        while (true)
+        {
+            Console.Write("Enter the size of the triangle: ");
+            if (int.TryParse(Console.ReadLine(), out int size) && size > 0)
+                return size;
+            Console.WriteLine("Please enter a positive integer.");
+        }
+    }
+}
