18da1832e0f80aabf0a92dc76b84528a037f56a8
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2024-04-02 00:40:58 +0300

    #Verilen string ifade içerisindeki karakterleri bir önceki karakter ile yer değiştiren console uygulamasını yazınız.

--- Algorithm/Models/Extension.cs
+++ Algorithm/Models/Extension.cs
@@ -2,6 +2,38 @@
 {
     private const char Space = ' ';
     private const char Comma = ',';
+    private const byte Zero = 0;
+    private const string StringEmpty = "";
+
+    public static string ReplacesThePreviousCharacter(this string @string)
+    {
+        string[] spaces = @string.Split(Space);
+        string sentence = StringEmpty, value;
+
+        foreach (string space in spaces)
+        {
+           
+            value = StringEmpty;
+            int spaceLength = space.Length;
+            string[] splits = new string[spaceLength];         
+
+            for (int i = Zero; i < spaceLength; i++)
+            {
+                int index = i + 1;
+
+                if (i == spaceLength - 1)
+                    splits[i] = Convert.ToString(space[Zero]);
+                else
+                    splits[i] = Convert.ToString(space[index]);
+            }
+
+            foreach (string split in splits)
+                value += split;
+
+            sentence += Space + value.Trim();
+        }
+        return sentence.Trim();
+    }
     public static string ExtractCharacterFromIndex(this string @string)
     {
         string value = string.Empty;
@@ -16,9 +48,9 @@
                 int index = int.Parse(splits[1]);
 
                 if (index <= (word.Length - 1))
-                    value += " " + word.Remove(index, 1);
+                    value += Space + word.Remove(index, 1);
                 else
-                    value += " " + word;
+                    value += Space + word;
             }
         }
         return value.Trim();
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -6,18 +6,27 @@
         Console.Write("Değer Gir : ");
         string input = Console.ReadLine();
 
-        Console.WriteLine(input.ExtractCharacterFromIndex());
-        Console.ReadKey();
+        //Console.WriteLine(input.ExtractCharacterFromIndex());
+        Console.WriteLine(input.ReplacesThePreviousCharacter());
 
+
+        Console.ReadKey();
     }
 
     /*
-Algoritma
-Ekrandan bir string bir de sayı alan (aralarında virgül ile), ilgili string ifade içerisinden verilen indexteki karakteri çıkartıp ekrana yazdıran console uygulasını yazınız.
+    Algoritma - 1
+    Ekrandan bir string bir de sayı alan (aralarında virgül ile), ilgili string ifade içerisinden verilen indexteki karakteri çıkartıp ekrana yazdıran console uygulasını yazınız.
 
-Örnek: Input: Algoritma,3 Algoritma,5 Algoritma,22 Algoritma,0
+    Örnek: Input: Algoritma,3 Algoritma,5 Algoritma,22 Algoritma,0
 
-Output: Algritma Algortma Algoritma lgoritma
+    Output: Algritma Algortma Algoritma lgoritma
 
-*/
+    Algoritma -2
+    Verilen string ifade içerisindeki karakterleri bir önceki karakter ile yer değiştiren console uygulamasını yazınız.
+
+    Örnek: Input: Merhaba Hello Question
+
+    Output: erhabaM elloH uestionQ
+
+    */
 }
