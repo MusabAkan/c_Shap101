bba4414291d7ccea9f478dbfb7055a1eddc90c39
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2024-04-07 13:12:20 +0300

    #MyDictionary çalışması

--- ConsoleApp1/MyList.cs
+++ ConsoleApp1/MyList.cs
@@ -1,5 +1,43 @@
 ﻿namespace GenericsIntro
 {
+    class MyDictionary<Key, Value>
+    {
+        Key[] _keys;
+        Value[] _values;
+        public MyDictionary()
+        {
+            _keys = new Key[0];
+            _values = new Value[0];
+        }
+
+        public void GetAllList()
+        {
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                Console.WriteLine($"Key [{i}] : {_keys[i]}, Value [{i}] : {_values[i]}");
+            }
+        }
+
+        public void Add(Key keys, Value values)
+        {
+            Key[] _tempKeys = _keys;
+            Value[] _tempValues = _values;
+
+            _keys = new Key[_keys.Length + 1];
+            _values = new Value[_values.Length + 1];
+
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                _keys[i] = _tempKeys[i];
+                _values[i] = _tempValues[i];
+            }
+
+            _keys[_keys.Length -1] = keys;
+            _values[_values.Length - 1] = values;
+        }
+    }
+
+
     class MyList<T>
     {
         T[] items;
@@ -17,7 +55,7 @@
         }
         public int Length
         {
-            get { return items.Length; } 
+            get { return items.Length; }
         }
         public T[] Items { get { return items; } }
 
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-       MyList<string>  isimler = new MyList<string>();
-        isimler.Add("Ayşe");
-        isimler.Add("Fatma");
-        isimler.Add("Denizli");
-        isimler.Add("Rize"); 
-        isimler.Read();
+
+        MyDictionary<string, int> dictionary = new();
+
+       
+
+
+        dictionary.Add("ayşe", 5);
+        dictionary.Add("teyze", 6);
+        dictionary.Add("Kayınço", 7);
+        dictionary.Add("lale", 8);
+
+        dictionary.GetAllList();
+
+        Console.ReadKey();
+
+
+        //MyList<string>  isimler = new MyList<string>();
+        // isimler.Add("Ayşe");
+        // isimler.Add("Fatma");
+        // isimler.Add("Denizli");
+        // isimler.Add("Rize"); 
+        // isimler.Read();
         //Console.WriteLine(isimler.ToString());
     }
 }
